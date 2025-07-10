48d01626765b445adbcbf30118d1ad41d0638fee
ricardo <nishimu75618094@gmail.com>
2025-07-10 23:27:41 +0900

    日付を追加

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update]
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
   def new
     @article = Article.new
@@ -20,11 +20,9 @@ class ArticlesController < ApplicationController
     end
   end
   def edit
-  @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to article_path(@article), notice: '更新できまいた'
     else
@@ -34,13 +32,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
-    @article.destroy!
+    article = Article.find(params[:id])
+    article.destroy!
     redirect_to root_path, status: :see_other, notice: '削除に成功しました'
   end
   #Strong Paramete
   private
   def article_params
     params.require(:article).permit(:title, :content)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
